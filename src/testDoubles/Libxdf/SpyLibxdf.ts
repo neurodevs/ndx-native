2ba1bb56bda62fed173e08ee57c36b15d6a8f848
ericthecurious <hello@ericthecurious.com>
2026-01-26 19:11:15 -0800

    minor: add adapters from node-xdf

---
+++ src/testDoubles/Libxdf/SpyLibxdf.ts
@@ -0,0 +1,13 @@
+import { MangledNameMap } from '@neurodevs/node-mangled-names'
+
+import LibxdfAdapter from '../../impl/LibxdfAdapter.js'
+
+export default class SpyLibxdf extends LibxdfAdapter {
+    public constructor(libxdfPath: string, mangledNameMap: MangledNameMap) {
+        super(libxdfPath, mangledNameMap)
+    }
+
+    public getBindings() {
+        return this.bindings
+    }
+}
