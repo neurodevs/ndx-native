dd416a5ce3b3ab23d76d3a63a36d9f37213313dd
ericthecurious <hello@ericthecurious.com>
2026-01-26 20:04:27 -0800

    patch: update exports

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,26 @@
-//exports go here
+// Labrecorder
+
+export { default as LabrecorderAdapter } from './impl/LabrecorderAdapter.js'
+export * from './impl/LabrecorderAdapter.js'
+
+export { default as FakeLabrecorder } from './testDoubles/Labrecorder/FakeLabrecorder.js'
+export * from './testDoubles/Labrecorder/FakeLabrecorder.js'
+
+// Liblsl
+
+export { default as LiblslAdapter } from './impl/LiblslAdapter.js'
+export * from './impl/LiblslAdapter.js'
+
+export { default as FakeLiblsl } from './testDoubles/Liblsl/FakeLiblsl.js'
+export * from './testDoubles/Liblsl/FakeLiblsl.js'
+
+// Libxdf
+
+export { default as LibxdfAdapter } from './impl/LibxdfAdapter.js'
+export * from './impl/LibxdfAdapter.js'
+
+export { default as FakeLibxdf } from './testDoubles/Libxdf/FakeLibxdf.js'
+export * from './testDoubles/Libxdf/FakeLibxdf.js'
+
+export { default as SpyLibxdf } from './testDoubles/Libxdf/SpyLibxdf.js'
+export * from './testDoubles/Libxdf/SpyLibxdf.js'
