8440579016e2dde97cb2cc4fbd4d477374a16de1
ericthecurious <hello@ericthecurious.com>
2026-01-26 19:23:46 -0800

    minor: add adapter from node-lsl

--- src/__tests__/AbstractPackageTest.ts
+++ src/__tests__/AbstractPackageTest.ts
@@ -1,6 +1,15 @@
 import AbstractModuleTest from '@neurodevs/node-tdd'
 
 export default abstract class AbstractPackageTest extends AbstractModuleTest {
+    protected static readonly channelNames = [
+        this.generateId(),
+        this.generateId(),
+        this.generateId(),
+    ]
+
+    protected static readonly channelCount = this.channelNames.length
+    protected static readonly chunkSize = 2
+
     protected static async beforeEach() {
         await super.beforeEach()
     }
